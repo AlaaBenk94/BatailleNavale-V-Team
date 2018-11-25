96fb3eacbe75aa98f0ee3d7a00879ced1b60f0b5
abdelaziz111 <azizcherifi10@gmail.com>
2018-11-25 01:20:21 +0100

    Plateau

--- src/Model/Bateaux/Bateau.java
+++ src/Model/Bateaux/Bateau.java
@@ -8,5 +8,24 @@ public abstract class Bateau {
 	private int force;
 	private int resistance;
 	private int projectiles;
+	private char etat; // état de la case du bateau : o -> occupée, t-> touchée, d ->detruite
+
+
+
+
+
+
+
+
+
+
+
+	public void setResistance(int resistance) {
+		this.resistance = resistance;
+	}
+
+	public int getResistance() {
+		return resistance;
+	}
 
 }
