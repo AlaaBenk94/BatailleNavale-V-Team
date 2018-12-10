fb9b5a3648492d35e748e04890b9a753d5457026
AlaaBenk94 <a.benkarrad@gmail.com>
2018-12-10 15:39:16 +0100

    deplacement des ressources

--- src/batailleNavale/Ressources.java
+++ src/batailleNavale/Ressources.java
@@ -8,4 +8,15 @@ public class Ressources {
     public static final int PLATEAU_WIDTH = 10;
     public static final int PLATEAU_HEIGHT = 10;
 
+    public static final int Largeur=10;
+    public static final int Hauteur=10;
+    public static final int CasTaille=30;
+    public static final int Largeurgrille=500;
+    public static final int Hauteurgrille=500;
+    public static final int fenetreLargeur=1200;
+    public static final int fenetreHeauteur=600;
+    public static final String menu_bg_img= "src/batailleNavale/img_ressource/bgmenu.png";
+    public static final String barre_bg_img= "src/batailleNavale/img_ressource/barrebg.png";
+    public static final String jeu_icon= "src/batailleNavale/img_ressource/jeuicon.png";
+    public static final String menu_icon= "src/batailleNavale/img_ressource/menuicon.png";
 }
