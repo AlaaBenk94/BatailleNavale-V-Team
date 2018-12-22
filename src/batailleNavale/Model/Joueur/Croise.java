9bdc73e27e81dbcddd5b47b7e630815d20490f71
AlaaBenk94 <a.benkarrad@gmail.com>
2018-12-22 22:58:19 +0100

    Croisé semi fini

--- src/batailleNavale/Model/Joueur/Croise.java
+++ src/batailleNavale/Model/Joueur/Croise.java
@@ -1,11 +1,46 @@
 package batailleNavale.Model.Joueur;
 
 import batailleNavale.Model.Bateaux.Bateau;
+import batailleNavale.Ressources;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static batailleNavale.Ressources.Hauteur;
+import static batailleNavale.Ressources.Largeur;
+import static batailleNavale.Ressources.TireEtats.TVide;
 
 public class Croise implements Tirer {
 
+    Random rand;
+    ArrayList<Point> PointsList;
+
+    public Croise() {
+        rand = new Random();
+    }
+
     @Override
     public boolean tirer(AbstractJoueur player, Bateau boat) {
+        int x,y;
+        Ressources.TireEtats etat;
+        x = rand.nextInt(Hauteur - 3);
+        y = rand.nextInt(Largeur - 3);
+        etat = player.prendreFeu(boat.tirer(x, y));
+
+        if(etat != TVide)
+            return true;
+
         return false;
+
+    }
+
+    private void initializeCross(){
+
+    }
+
+    public static Croise getInstance(){
+        return new Croise();
     }
 }
